7fd99c92df618cb7cab2318f9b02c9b8f4b36216
Amirk22 <amirhossein27q@gmail.com>
2026-02-15 13:23:25 -0800

    chore: add Django REST framework to project

--- marketplace_api/settings.py
+++ marketplace_api/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+INSTALLED_APPS +=[
+    'rest_framework',
+]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
