1b5d7bcc1a0848ed2405bb030282e7eef9bf71f2
Amirk22 <amirhossein27q@gmail.com>
2026-02-20 04:55:33 -0800

    feat: add serializers for product, category, brand, and color

---
+++ products/serializers.py
@@ -0,0 +1,31 @@
+from rest_framework import serializers
+from .models import *
+
+class ProductSerializer(serializers.ModelSerializer):
+    vendor = serializers.PrimaryKeyRelatedField(read_only=True)
+    class Meta:
+        model = Product
+        fields = '__all__'
+        read_only_fields = ('id','created_at','updated_at')
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+        read_only_fields = ('id',)
+
+
+
+class BrandSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Brand
+        fields = '__all__'
+        read_only_fields = ('id',)
+
+
+class ColorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Color
+        fields = '__all__'
+        read_only_fields = ('id',)
