fae434508b6ec6cac2c2a21b132caa066458159b
Amirk22 <amirhossein27q@gmail.com>
2026-02-20 04:02:41 -0800

    fix: correct vendor request url typo

--- accounts/urls.py
+++ accounts/urls.py
@@ -11,9 +11,9 @@ urlpatterns = [
     path('password/forgot/',ForgetPasswordView.as_view(),name='forget_password'),
     path('password/verify/',VerifyForgetPasswordView.as_view(),name='verify_forget_password'),
     path('password/reset/',ChangePasswordView.as_view(),name='password_change'),
-    path('vender/request/',VendorRequestView.as_view(),name='vender_request'),
+    path('vendor/request/',VendorRequestView.as_view(),name='request_vendor'),
     path('vendor/active/',ActiveVendorView.as_view(),name='active_vendor'),
     path('vendor/unactive/',UnactiveVendorView.as_view(),name='unactive_vendor'),
-    path('vendor/<int:pk>',AdminVendorApproveView.as_view(),name='admin_vendor_approve'),
+    path('vendor/<int:pk>/',AdminVendorApproveView.as_view(),name='admin_vendor_approve'),
     path('profile/',ProfileView.as_view(),name='profile'),
 ]
