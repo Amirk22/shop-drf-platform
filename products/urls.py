c4a71857823973048d1505c99a2a3a6a785f06a5
Amirk22 <amirhossein27q@gmail.com>
2026-02-20 04:56:35 -0800

    feat: add product, category, brand, and color views with pagination and permissions

--- products/urls.py
+++ products/urls.py
@@ -1,3 +1,17 @@
 from django.urls import path
+from .views import *
 
-urlpatterns = []
+urlpatterns = [
+    path('',ProductView.as_view(),name='products'),
+    path('<int:pk>/',ProductDetailView.as_view(),name='product_detail'),
+    path('create/',CreateProductView.as_view(),name='product_create'),
+    path('category/',CategoryView.as_view(),name='categories'),
+    path('category/<int:pk>/',CategoryDetailView.as_view(),name='category_detail'),
+    path('category/create/',CreateCategoryView.as_view(),name='category_create'),
+    path('brand/',BrandView.as_view(),name='brands'),
+    path('brand/<int:pk>/',BrandDetailView.as_view(),name='brand_detail'),
+    path('brand/create/',CreateBrandView.as_view(),name='create_brand'),
+    path('color/',ColorView.as_view(),name='colors'),
+    path('color/create/',CreateColorView.as_view(),name='color_create'),
+    path('color/<int:pk>/',ColorDetailView.as_view(),name='color_detail'),
+]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,97 @@
-from django.shortcuts import render
-
+from rest_framework import generics
+from .serializers import *
+from accounts.models import VendorProfile
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import IsAdminUser
+from accounts.views import IsVendor , IsVendorOrAdmin
 # Create your views here.
+
+
+# Pagination
+
+class ProductPagination(PageNumberPagination):
+    page_size = 24
+class GeneralPagination(PageNumberPagination):
+    page_size = 12
+
+# Product
+
+class ProductView(generics.ListAPIView):
+    serializer_class = ProductSerializer
+    pagination_class = ProductPagination
+    def get_queryset(self):
+        return Product.objects.filter(is_active=True).select_related(
+            'vendor', 'category', 'brand', 'color'
+        )
+
+class CreateProductView(generics.CreateAPIView):
+    permission_classes = [IsVendor]
+    serializer_class = ProductSerializer
+
+    def perform_create(self,serializer):
+        vendor = VendorProfile.objects.get(
+            user=self.request.user,
+            is_approved=True
+        )
+        serializer.save(vendor=vendor)
+
+class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = ProductSerializer
+    permission_classes = [IsVendorOrAdmin]
+
+    def get_queryset(self):
+        if self.request.user.is_superuser:
+            return Product.objects.all()
+
+        return Product.objects.filter(
+            vendor__user=self.request.user
+        )
+
+# Category
+
+class CategoryView(generics.ListAPIView):
+    serializer_class = CategorySerializer
+    pagination_class = GeneralPagination
+    def get_queryset(self):
+        return Category.objects.all()
+
+class CreateCategoryView(generics.CreateAPIView):
+    permission_classes = [IsAdminUser]
+    serializer_class = CategorySerializer
+
+class CategoryDetailView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAdminUser]
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
+
+# Brand
+
+class BrandView(generics.ListAPIView):
+    serializer_class = BrandSerializer
+    pagination_class = GeneralPagination
+    queryset =  Brand.objects.all()
+
+class CreateBrandView(generics.CreateAPIView):
+    permission_classes = [IsAdminUser]
+    serializer_class = BrandSerializer
+
+class BrandDetailView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAdminUser]
+    serializer_class = BrandSerializer
+    queryset = Brand.objects.all()
+
+# color
+
+class ColorView(generics.ListAPIView):
+    serializer_class = ColorSerializer
+    pagination_class = GeneralPagination
+    queryset = Color.objects.all()
+
+class CreateColorView(generics.CreateAPIView):
+    permission_classes = [IsAdminUser]
+    serializer_class = ColorSerializer
+
+class ColorDetailView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAdminUser]
+    serializer_class = ColorSerializer
+    queryset = Color.objects.all()
