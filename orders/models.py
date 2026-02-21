7c69e8ca1bbc7f40e7f7eabf8c6be2a2006fcf18
Amirk22 <amirhossein27q@gmail.com>
2026-02-21 14:20:43 -0800

    feat: add Cart and CartItem models

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from products.models import Product
+from accounts.models import User
 
 # Create your models here.
+
+class Cart(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE,null=True,blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def count_items(self):
+        return self.items.count()
+
+    def total_price(self):
+        return sum(item.total_price() for item in self.items.all())
+
+    def __str__(self):
+        return f"Cart {self.id}"
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart,related_name="items",on_delete=models.CASCADE)
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def total_price(self):
+        return self.product.price * self.quantity
+
+    class Meta:
+        unique_together = ('cart', 'product')
